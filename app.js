8db1cf6a07e5878d33a685b672157f432ea36821
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-28 12:31:21 +0530

    oreder.js linked to app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 
 const productRoutes = require("./api/routes/products");
+const orerRoutes = require("./api/routes/orders");
+
 app.use("/products", productRoutes);
+app.use("/orders", orerRoutes);
 
 module.exports = app;
