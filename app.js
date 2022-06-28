39c201e468bb66b58f37a5727ec0610c84282180
Darshana Perera <darshana.saluka.10@gmail.com>
2022-06-28 12:49:20 +0530

    import morgan to app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const orerRoutes = require("./api/routes/orders");
+
+app.use(morgan("dev"));
 
 app.use("/products", productRoutes);
 app.use("/orders", orerRoutes);
